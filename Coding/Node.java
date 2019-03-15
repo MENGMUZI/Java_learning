e6356c2e51bb023ca28ba40f26d37168029a8cf8
孟木子 <18351923886@163.com>
2019-03-15 22:51:16 +0800

    一种特殊的链表节点

---
+++ Coding/Node.java
@@ -0,0 +1,13 @@
+/**
+ * @author : mengmuzi
+ * create at:  2019-03-15  21:34
+ * @description: 一种特殊的链表节点
+ */
+public class Node {
+    public int data;
+    public Node next;
+    public Node rand;
+    public Node(int data){
+        this.data = data;
+    }
+}
