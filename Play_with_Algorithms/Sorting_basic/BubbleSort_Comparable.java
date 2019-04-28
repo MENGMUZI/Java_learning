0617699b51f3ddb5bff927b1a81b49fb9dce79b7
孟木子 <18351923886@163.com>
2019-04-28 11:59:20 +0800

    冒泡排序

---
+++ Play_with_Algorithms/Sorting_basic/BubbleSort_Comparable.java
@@ -0,0 +1,35 @@
+package Sorting_basic;
+
+/**
+ * @author : mengmuzi
+ * create at:  2019-04-28  11:16
+ * @description: 冒泡排序
+ */
+public class BubbleSort_Comparable {
+
+    // 我们的算法类不允许产生任何实例
+    private BubbleSort_Comparable(){}
+
+    public static void sort(Comparable[] arr){
+        int n = arr.length;
+        boolean swapped = false;
+        do{
+            swapped = false;
+            for(int i = 1; i < n ; i++){
+                if(arr[i-1].compareTo(arr[i]) > 0){
+                    swap(arr, i-1, i);
+                    swapped = true;
+                }
+            }
+            // 优化, 每一趟Bubble Sort都将最大的元素放在了最后的位置
+            // 所以下一次排序, 最后的元素可以不再考虑
+            n --;
+        }while(swapped);
+    }
+    private static void swap(Object[] arr, int i, int j) {
+        Object temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+}
